b051855abc30296bd4639e1b865b3aa5db6c0761
JAVA-08 <vrsa9208@gmail.com>
2016-02-12 20:42:53 -0600

    Cargando archivos al repo

---
+++ src/java/servlets/Saludo.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author JAVA-08
+ */
+public class Saludo extends HttpServlet {
+
+    //localhost:8080/Test/saludo?nombre=orlando
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String parametro = request.getParameter("nombre");
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<style>li{color:red}</style>");
+            out.println("<title>Servlet Saludo</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Saludo at " + request.getContextPath() + "</h1>");
+            
+            if(parametro != null && !parametro.isEmpty()){
+                out.println("<p>Tu nombre es: " + parametro + "</p>");
+            }
+            
+            Enumeration<String> nombres = request.getParameterNames();
+            String listaParametro = "<h3>Lista de Parametros</h3><ul>";
+            while(nombres.hasMoreElements()){
+                String nombreParametro = nombres.nextElement();
+                String valorParam = request.getParameter(nombreParametro);
+                listaParametro += "<li>" + nombreParametro + " - " + valorParam + "</li>";
+            }
+            listaParametro += "</ul>";
+            out.println(listaParametro);
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : index
+    Created on : 12/02/2016, 06:09:52 PM
+    Author     : JAVA-08
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <h2>${2 * 2 + 3}</h2>
+        
+    </body>
+</html>
